64b925b873b1f22a2db7597732e2989d6868c630
Jimin Park <pjm950@gmail.com>
2015-05-29 19:25:04 +0900

    Changed DB to CSV

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # KDD
 ## 데이터 준비
+### csv
+`data/raw`에 `csv` 파일들을 저장한다.
+
+### PostgreSQL
 PostgreSQL 서버를 띄우고, `postgres` 사용자의 비밀번호를 `postgres`로 맞춘다. (`src`의 코드를 직접 수정해도 됨)
 
 `psql postgres postgres`로 PostgreSQL 서버에 접속하고, `create database Kdd2013AuthorPaperIdentification`로 데이터베이스를 생성한다.
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,26 +1,15 @@
 #!/usr/bin/env python
 
-import psycopg2
+import csv
 
-copaper = dict()
-coauthor = dict()
+def getCSV(file_name):
+	reader = csv.reader(open("../data/raw/%s.csv" % file_name, 'r'))
+	reader.next() # ignore column titles
+	return reader
 
-conn = psycopg2.connect("dbname=kdd2013authorpaperidentification user=postgres password=postgres")
-cur = conn.cursor()
-cur.execute("""SELECT paperid, authorid FROM paperauthor;""")
+csv_PaperAuthor = getCSV("PaperAuthor")
 
-print("Filling copapers and coauthors...")
-for row in cur.fetchall():
-	if row[1] in copaper:
-		copaper[row[1]].append(row[0])
-	else:
-		copaper[row[1]] = [row[0]]
-	if row[0] in coauthor:
-		coauthor[row[0]].append(row[1])
-	else:
-		coauthor[row[0]] = [row[1]]
+i = 0
 
-print("Done.")
-
-cur.close()
-conn.close()
+for PaperId, AuthorId, Name, Affiliation in csv_PaperAuthor:
+	pass
