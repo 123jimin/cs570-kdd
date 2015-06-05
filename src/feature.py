f487e7251183454ff8fb723d2da1afbca5e5677e
Jimin Park <pjm950@gmail.com>
2015-06-05 14:06:10 +0900

    Added more results

--- src/feature.py
+++ src/feature.py
@@ -9,66 +9,38 @@ def getFeatureCSV(file_name, header=True):
 
 dict_features_pa = dict()
 dict_features_a = dict()
+set_keys_tmp = set()
 
-set_keys = set()
+def addFeatures(file_name, header=True, rev=False):
+	set_keys_tmp.clear()
+	for row in getFeatureCSV(file_name, header):
+		key = None
+		if rev:
+			key = (int(row[1]), int(row[0]))
+		else:
+			key = (int(row[0]), int(row[1]))
+		if key not in set_keys_tmp:
+			set_keys_tmp.add(key)
+			if key not in dict_features_pa:
+				dict_features_pa[key] = list()
+			if file_name == "feature_test":
+				# ignore pair feature
+				dict_features_pa[key].extend(map(float, row[3:]))
+			else:
+				dict_features_pa[key].extend(map(float, row[2:]))
 
-for row in getFeatureCSV("paperauthor_author_feature"):
-	key = (int(row[0]), int(row[1]))
-	if key not in set_keys:
-		set_keys.add(key)
-		dict_features_pa[key] = map(float, row[2:])
-
-set_keys.clear()
+set_keys_tmp.clear()
 
 for row in getFeatureCSV("feature1"):
-	if int(row[0]) not in set_keys:
-		set_keys.add(int(row[0]))
+	if int(row[0]) not in set_keys_tmp:
+		set_keys_tmp.add(int(row[0]))
 		dict_features_a[int(row[0])] = map(float, row[1:])
 
-set_keys.clear()
-
-for row in getFeatureCSV("feature2"):
-	key = (int(row[1]), int(row[0]))
-	if key in dict_features_pa and key not in set_keys:
-		set_keys.add(key)
-		dict_features_pa[key].extend(map(float, row[2:]))
-
-set_keys.clear()
-
-for row in getFeatureCSV("feature4"):
-	key = (int(row[1]), int(row[0]))
-	if key in dict_features_pa and key not in set_keys:
-		set_keys.add(key)
-		dict_features_pa[key].extend(map(float, row[2:]))
-
-set_keys.clear()
-
-for row in getFeatureCSV("feature_HBN", False):
-	key = (int(row[0]), int(row[1]))
-	if key in dict_features_pa and key not in set_keys:
-		set_keys.add(key)
-		dict_features_pa[key].extend(map(float, row[2:]))
-
-set_keys.clear()
-n = 0
-o = None
-k = None
-
-for row in getFeatureCSV("feature_test"):
-	key = (int(row[0]), int(row[1]))
-	# dict_features_pa[key] = [int(row[2])]
-	dict_features_pa[key].extend(map(float, row[3:]))
-	if n == 0:
-		o = dict_features_pa[key]
-		n = len(o)
-		k = key
-	elif n != len(dict_features_pa[key]):
-		print("%d %d" % (n, len(dict_features_pa[key])))
-		print(k)
-		print(o)
-		print(key)
-		print(dict_features_pa[key])
-		raise Exception("Feature lengths differ")
+addFeatures("paperauthor_author_feature_cut")
+addFeatures("feature2_cut")
+addFeatures("feature4_cut")
+addFeatures("feature_HBN_cut", header=False)
+addFeatures("feature_test")
 
 def getFeature(AuthorId, PaperId):
 	key = (PaperId, AuthorId)
